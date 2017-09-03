0f8640171a53716ed5295e5b4433a36171b9ff57
tuuzed <liuyonghui.job@gmail.com>
2017-09-03 10:38:30 +0800

    新增: 可直接传入url或输入流解析Feed

--- src/main/java/com/tuuzed/feedparser/FeedParser.java
+++ src/main/java/com/tuuzed/feedparser/FeedParser.java
@@ -6,19 +6,96 @@ import com.tuuzed.feedparser.internal.RssParser;
 import com.tuuzed.feedparser.util.DateParser;
 import com.tuuzed.feedparser.util.FastXmlPullParser;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.Reader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FeedParser {
+    private static Pattern CHARSET_PATTERN = Pattern.compile(
+            "(encoding|charset)=.*(GB2312|UTF-8|GBK).*",
+            Pattern.CASE_INSENSITIVE);
 
     public static void appendDateFormat(@NotNull DateFormat format) {
         DateParser.appendDateFormat(format);
     }
 
+    public static void parse(@NotNull String url, @NotNull FeedCallback callback) {
+        parse(url, callback, "utf-8");
+    }
+
+    public static void parse(@NotNull String url, @NotNull FeedCallback callback,
+                             @NotNull String defCharset) {
+        InputStream input = null;
+        Reader reader = null;
+        String charset = null;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            final String contentType = connection.getContentType();
+            if (contentType != null) {
+                Matcher matcher = CHARSET_PATTERN.matcher(contentType);
+                if (matcher.find()) {
+                    charset = matcher.group(2);
+                }
+            }
+            input = connection.getInputStream();
+            // 没有匹配到编码
+            if (charset == null) parse(input, callback, defCharset);
+                // 匹配到编码
+            else {
+                reader = new InputStreamReader(input, charset);
+                parse(reader, callback);
+            }
+
+        } catch (IOException e) {
+            callback.fatalError(e);
+        } finally {
+            safeClose(reader);
+            safeClose(input);
+        }
+    }
+
+    public static void parse(@NotNull InputStream input, @NotNull FeedCallback callback) {
+        parse(input, callback, "utf-8");
+    }
+
+    public static void parse(@NotNull InputStream input, @NotNull FeedCallback callback,
+                             @NotNull String defCharset) {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Reader reader = null;
+        final byte[] buffer = new byte[2048];
+        String charset = null;
+        try {
+            int len;
+            while ((len = input.read(buffer)) != -1) {
+                output.write(buffer, 0, len);
+            }
+            output.flush();
+            byte[] bytes = output.toByteArray();
+            String content = new String(bytes);
+            Matcher matcher = CHARSET_PATTERN.matcher(content);
+            if (matcher.find()) {
+                charset = matcher.group(2);
+            }
+            if (charset == null) {
+                charset = defCharset;
+            }
+            reader = new InputStreamReader(new ByteArrayInputStream(bytes), charset);
+            parse(reader, callback);
+        } catch (IOException e) {
+            callback.fatalError(e);
+        } finally {
+            safeClose(output);
+            safeClose(reader);
+        }
+    }
 
     public static void parse(@NotNull Reader reader, @NotNull final FeedCallback callback) {
         try {
@@ -56,6 +133,17 @@ public class FeedParser {
             });
         } catch (XmlPullParserException e) {
             callback.fatalError(e);
+        }
+    }
+
+
+    private static void safeClose(@Nullable Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                // pass
+            }
         }
     }
 
--- src/test/java/com/tuuzed/feedparser/FeedParserTest.java
+++ src/test/java/com/tuuzed/feedparser/FeedParserTest.java
@@ -1,43 +1,23 @@
 package com.tuuzed.feedparser;
 
-import okhttp3.*;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.net.URL;
 
 
 public class FeedParserTest {
-    private OkHttpClient httpClient;
-
-    @Before
-    public void setup() {
-        httpClient = new OkHttpClient.Builder().build();
-    }
-
-    private ResponseBody getResponseBody(String url) {
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .build();
-        Call call = httpClient.newCall(request);
-        try {
-            Response response = call.execute();
-            return response.body();
-        } catch (IOException e) {
-            return null;
-        }
-    }
 
     @Test
     public void rss() throws Exception {
-        String rss = "http://news.qq.com/newsgn/rss_newsgn.xml";
-        ResponseBody responseBody = getResponseBody(rss);
-        if (responseBody != null) {
-            FeedParser.parse(responseBody.charStream(), new FeedCallbackImpl());
-        }
+        FeedParser.parse("http://epayment.blog.sohu.com/rss", new FeedCallbackImpl());
+        FeedParser.parse("http://paynews.net/portal.php?mod=rss", new FeedCallbackImpl());
+        FeedParser.parse("http://news.qq.com/newsgn/rss_newsgn.xml", new FeedCallbackImpl());
+    }
+
+    @Test
+    public void atom() throws Exception {
+        FeedParser.parse("https://www.v2ex.com/feed/tab/tech.xml", new FeedCallbackImpl());
     }
 
     @Test
@@ -48,14 +28,6 @@ public class FeedParserTest {
         }
     }
 
-    @Test
-    public void atom() throws Exception {
-        String atom = "https://www.v2ex.com/feed/tab/tech.xml";
-        ResponseBody responseBody = getResponseBody(atom);
-        if (responseBody != null) {
-            FeedParser.parse(responseBody.charStream(), new FeedCallbackImpl());
-        }
-    }
 
     @Test
     public void localAtom() throws Exception {
